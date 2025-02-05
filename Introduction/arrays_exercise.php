b0128b753b3a53e9714713e7c3188ea7af9617b3
Jimmy Muzamuzi <jimmymugisha55@gmail.com>
2025-02-05 12:48:45 +0300

    Updated feature second_exercise and arrays_exercise

--- Introduction/arrays_exercise.php
+++ Introduction/arrays_exercise.php
@@ -11,12 +11,12 @@ function print_array($a = [])
     foreach ($a as $num) {
         echo $num . ",";
     }
+    echo "<br/>";
 }
 
 function push_array($val, $a = [])
 {
-    $length = sizeof($a);
-    $a[$length] = $val;
+    $a[sizeof($a)] = $val;
     return $a;
 }
 
@@ -48,34 +48,101 @@ function exists_in_array($element, $array = [])
     return $result;
 }
 
-function exists_text($a,$b=[]){
+function exists_text($a, $b = [])
+{
     if (exists_in_array($a, $b)) {
-        echo "$a is in the array [";
+        echo "$a is in the array ";
         print_array($b);
-        echo "].";
     } else {
-        echo "$a is not in the array [";
+        echo "$a is not in the array ";
         print_array($b);
-        echo "].";
     }
 }
 
-function pop_array($a = []){
-    for($i=0;$i<(sizeof($a)-1);$i++){
+$m = [2, 4, 7, 8]; //3 4  n < 3 // 
+function pop_array($a = [])
+{
+    for ($i = 0; $i < (sizeof($a)); $i++) {
         $result[$i] = $a[$i];
     }
     return $result;
 }
+function sort_any($a, $array = [])
+{
+    if ($a === 'asc') {
+        $length = count($array);
 
+        for ($i = 0; $i < $length - 1; $i++) {
+            for ($j = 0; $j < $length - $i - 1; $j++) {
+                if ($array[$j] > $array[$j + 1]) { // Swap if current is smaller than next
+                    $temp = $array[$j];
+                    $array[$j] = $array[$j + 1];
+                    $array[$j + 1] = $temp;
+                }
+            }
+        }
+
+        return $array;
+    } elseif ($a === 'desc') {
+        $length = count($array);
+
+        for ($i = 0; $i < $length - 1; $i++) {
+            for ($j = 0; $j < $length - $i - 1; $j++) {
+                if ($array[$j] < $array[$j + 1]) { // Swap if current is smaller than next
+                    $temp = $array[$j];
+                    $array[$j] = $array[$j + 1];
+                    $array[$j + 1] = $temp;
+                }
+            }
+        }
+
+        return $array;
+    }
+}
+function sort_d($array)
+{
+    $length = count($array);
+
+    for ($i = 0; $i < $length - 1; $i++) {
+        for ($j = 0; $j < $length - $i - 1; $j++) {
+            if ($array[$j] < $array[$j + 1]) {
+                $temp = $array[$j];
+                $array[$j] = $array[$j + 1];
+                $array[$j + 1] = $temp;
+            }
+        }
+    }
+
+    return $array;
+}
+
+function sort_a($array)
+{
+    $length = count($array);
+
+    for ($i = 0; $i < $length - 1; $i++) {
+        for ($j = 0; $j < $length - $i - 1; $j++) {
+            if ($array[$j] > $array[$j + 1]) {
+                $temp = $array[$j];
+                $array[$j] = $array[$j + 1];
+                $array[$j + 1] = $temp;
+            }
+        }
+    }
+
+    return $array;
+}
+
+//Push Function Output
 $push = array(1, 2, 3, 4, 5, 6, 7, 8, 9);
 echo "<h4>Array before push</h4>";
 print_array($push);
-
 
 echo "<h4>Array after push</h4>";
 $result = push_array(10, $push);
 print_array($result);
 
+//Flip Function Output
 $flip = array("suv" => "Urus", "sports" => "Huracan", "coupe" => "Aventador");
 echo "<h4>Unflipped Associative Array</h4>";
 print_r($flip);
@@ -86,22 +153,59 @@ print_r($flipped);
 
 echo "<br/>";
 
+//In is array Function Output
 $in_array = array(1, 2, 3, 4, 5, 6, 7, 8, 9);
 $element = 2;
 
 echo "<h4>Exists in Array</h4>";
-exists_text($element,$in_array);
+exists_text($element, $in_array);
 
 echo "<br/>";
 $element = 16;
-exists_text($element,$in_array);
+exists_text($element, $in_array);
 
+//Pop Function Output
 echo "<br/>";
-$popped = [1,2,3,4,5,6,7,8,9,10];
+$popped = [1, 2, 3, 4, 5, 6, 7, 8, 9];
 echo "<h4>Array before pop</h4>";
-print_array($popped);
+print_r($popped);
 
 $pop = pop_array($popped);
 echo "<h4>Array after pop</h4>";
 print_array($pop);
+
+//Sort Function Output
+echo "<h4>Array before sort</h4>";
+$sort_a = array(9, 4, 7, 2, 6, 1, 5, 8, 3);
+$sort_b = array('j', 'b', 'd', 'i', 'f', 'a', 'h', 'e', 'g', 'c');
+$sort_c = array('G', 'B', 'I', 'F', 'H', 'E', 'A', 'D', 'J', 'C');
+
+print_array($sort_a);
+print_array($sort_b);
+print_array($sort_c);
+
+echo "<h4>Array after sort (descending order)</h4>";
+$sort_a = sort_d($sort_a);
+$sort_b = sort_d($sort_b);
+$sort_c = sort_d($sort_c);
+print_array($sort_a);
+print_array($sort_b);
+print_array($sort_c);
+
+echo "<h4>Array after sort (ascending order)</h4>";
+$sort_a = sort_a($sort_a);
+$sort_b = sort_a($sort_b);
+$sort_c = sort_a($sort_c);
+print_array($sort_a);
+print_array($sort_b);
+print_array($sort_c);
+
+//single function that accepts two arguments and can change sort order
+$sort_d = array(8,12,24,46,64,12,42,10,88,12);
+
+$sort_d = sort_any('desc',$sort_d);
+print_array($sort_d);
+
+$sort_d = sort_any('asc',$sort_d);
+print_array($sort_d);
 ?>
--- Introduction/second_exercise.php
+++ Introduction/second_exercise.php
@@ -1,72 +1,88 @@
-<?php 
+<?php
 
-function br() {
+function br()
+{
     echo "<br/>";
 }
-function no_vowels($a){
-    if (is_string($a)){
-        $length=strlen($a);
-        $chars = explode("",$a);
-        $vowels=0;
+function print_array($a = [])
+{
+    foreach ($a as $num) {
+        echo $num . ",";
+    }
+    echo "<br/>";
+}
+function no_vowels($a)
+{
+    if (is_string($a)) {
+        $length = strlen($a);
+        $chars = explode("", $a);
+        $vowels = 0;
 
-        for($i=0;$i<$length;$i++){
-            if($chars[$i]=="a" || $chars[$i]=="e" || $chars[$i]=="i" || $chars[$i]=="o" || $chars[$i]=="u"){
+        for ($i = 0; $i < $length; $i++) {
+            if ($chars[$i] == "a" || $chars[$i] == "e" || $chars[$i] == "i" || $chars[$i] == "o" || $chars[$i] == "u") {
                 $vowels++;
             }
         }
-
         return $vowels;
-    }else{
+    } else {
         return "$a is not a string!";
     }
 }
+function sort_d($array)
+{
+    $length = count($array);
 
-function max($a){
-    $max = 0;
-
-    foreach($a as $b){
-        if($max<$b){
-            $max=$b;
-        }
-    }
-
-    return $max;
-}
-
-function min($a){
-    
-    $min = 0;
-    foreach($a as $b){
-        if($min>$b){
-            $min=$b;
-        }
-    }
-    return $min;
-}
-
-function sort_desc($b){
-
-    if(is_array($b)){
-        $length = sizeof($b);
-        $c = $b;
-        $num=0;
-
-        for($i=0;$i<$length;$i++){
-            if($c[0]==min($c)){
-                array_shift($c);
-            }else{
-                $temp=$c[0];
-                array_shift($c);
-                array_push($c,$temp);
+    for ($i = 0; $i < $length - 1; $i++) {
+        for ($j = 0; $j < $length - $i - 1; $j++) {
+            if ($array[$j] < $array[$j + 1]) { // Swap if current is smaller than next
+                $temp = $array[$j];
+                $array[$j] = $array[$j + 1];
+                $array[$j + 1] = $temp;
             }
         }
-
-    }else{
-        return "$b is not an  array!";
     }
+
+    return $array; // Return the sorted array
+}
+function sort_a($array)
+{
+    $length = count($array);
+
+    for ($i = 0; $i < $length - 1; $i++) {
+        for ($j = 0; $j < $length - $i - 1; $j++) {
+            if ($array[$j] > $array[$j + 1]) { // Swap if current is smaller than next
+                $temp = $array[$j];
+                $array[$j] = $array[$j + 1];
+                $array[$j + 1] = $temp;
+            }
+        }
+    }
+
+    return $array; // Return the sorted array
 }
 
-function reverse($c){
+echo "<h4>Array before sort</h4>";
+$sort_a = array(9, 4, 7, 2, 6, 1, 5, 8, 3);
+$sort_b = array('j', 'b', 'd', 'i', 'f', 'a', 'h', 'e', 'g', 'c');
+$sort_c = array('G', 'B', 'I', 'F', 'H', 'E', 'A', 'D', 'J', 'C');
 
-}
+print_array($sort_a);
+print_array($sort_b);
+print_array($sort_c);
+
+echo "<h4>Array after sort (descending order)</h4>";
+$sort_a = sort_d($sort_a);
+$sort_b = sort_d($sort_b);
+$sort_c = sort_d($sort_c);
+print_array($sort_a);
+print_array($sort_b);
+print_array($sort_c);
+
+echo "<h4>Array after sort (ascending order)</h4>";
+$sort_a = sort_a($sort_a);
+$sort_b = sort_a($sort_b);
+$sort_c = sort_a($sort_c);
+print_array($sort_a);
+print_array($sort_b);
+print_array($sort_c);
 ?>
